b468e27b7fa2a09fd3aa092e86da9568fff113e2
Christine Savadel <1872530+chrissav@users.noreply.github.com>
2019-03-28 07:24:06 -0400

    Update deploy.sh

--- tenable-to-jira/deploy.sh
+++ tenable-to-jira/deploy.sh
@@ -3,5 +3,5 @@ rm archive.zip
 zip -r9 archive.zip .
 cd lib/python3.6/site-packages/ && zip -r9 ../../../archive.zip .
 cd ../../../
-aws s3 mv archive.zip s3://2u-devops/lambda/$dir.zip
-aws lambda update-function-code --function-name $dir --region us-west-2 --s3-bucket 2u-devops --s3-key lambda/$dir.zip
+aws s3 mv archive.zip s3://$S3BUCKET/lambda/$dir.zip
+aws lambda update-function-code --function-name $dir --region us-west-2 --s3-bucket $S3BUCKET --s3-key lambda/$dir.zip
