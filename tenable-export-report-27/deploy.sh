dd35658c7bc70afa3f55a715373898ed6a820de3
Christine Savadel <1872530+chrissav@users.noreply.github.com>
2019-03-28 07:26:18 -0400

    Update deploy.sh

--- tenable-export-report-27/deploy.sh
+++ tenable-export-report-27/deploy.sh
@@ -2,5 +2,5 @@ rm archive.zip
 zip -r9 archive.zip .
 cd lib/python2.7/site-packages/ && zip -r9 ../../../archive.zip .
 cd ../../../
-aws s3 mv archive.zip s3://2u-devops/lambda/tenable-export-report.zip
-aws lambda update-function-code --function-name tenable-export-report --region us-west-2 --s3-bucket 2u-devops --s3-key lambda/tenable-export-report.zip
+aws s3 mv archive.zip s3://$S3BUCKET/lambda/tenable-export-report.zip
+aws lambda update-function-code --function-name tenable-export-report --region us-west-2 --s3-bucket $S3BUCKET --s3-key lambda/tenable-export-report.zip
